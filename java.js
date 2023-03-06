065421b1b9ff74f87daba44bf909c8817000dcea
ryonobitches <121812233+ryonobitches@users.noreply.github.com>
2023-03-06 20:44:25 -0300

    Create java.js

---
+++ java.js
@@ -0,0 +1,15 @@
+const botao = document.getElementById("btn"); addEventListener("click", add)
+
+function add(){
+    alert("Verificando número de telefone...")
+    alert("Concluindo...")
+    alert("Conta criada com sucesso!✅")
+}
+
+const botao2 = document.getElementById("bttn"); addEventListener("click", add)
+
+function add(){
+    alert("Verificando número de telefone...")
+    alert("Concluindo...")
+    alert("Conta criada com sucesso!✅")
+}
